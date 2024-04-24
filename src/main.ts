199fa972662699a46fe203404800169a2e14c510
Ashraf Khan <2094ashrafkhan@gmail.com>
2024-04-24 12:00:47 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'],
+  });
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
