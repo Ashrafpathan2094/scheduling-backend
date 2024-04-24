717b51ce0e21f0f34807f513a07b2597417d7e5d
Ashraf Khan <2094ashrafkhan@gmail.com>
2024-04-24 11:08:06 +0530

    reset cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule, { cors: true });
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://scheduling-frontend-2094ashraf.vercel.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  app.enableCors();
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
