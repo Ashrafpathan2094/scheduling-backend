90a201429a511bf01968cf00e63582bf6503c3b3
Ashraf Khan <2094ashrafkhan@gmail.com>
2024-04-24 11:29:27 +0530

    cors reset final

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, { cors: true });
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
