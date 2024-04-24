30f7405d0b04969ed3f18fb1c8cd007e0729ba9d
Ashraf Khan <2094ashrafkhan@gmail.com>
2024-04-24 12:22:27 +0530

    removed pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
-import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  app.useGlobalPipes(new ValidationPipe());
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
