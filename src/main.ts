658fd24a4809a1a1ed1528eb3f981e3f788704f0
Ashraf Khan <2094ashrafkhan@gmail.com>
2024-04-24 11:12:31 +0530

    allow all cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule, { cors: true });
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 200,
+    allowedHeaders: '*',
+  });
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
