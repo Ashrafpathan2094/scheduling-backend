10a76f1c5261db7c1f4ef2d77cbd27b1c3d441d5
Ashraf Khan <2094ashrafkhan@gmail.com>
2024-04-24 12:15:25 +0530

    pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: 'https://scheduling-frontend-2094ashraf.vercel.app',
-    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'],
-  });
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
